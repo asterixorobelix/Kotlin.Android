49433e0b27058ad26c5681c07902f2469ddd6da8
asterixorobelix <nathanstasin@gmail.com>
2018-11-25 20:57:32 +0200

    add drawable xmls for dice and swap them out when clicked

--- diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
+++ diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
@@ -3,8 +3,8 @@ package com.example.dell.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class DiceRoller : AppCompatActivity() {
@@ -23,9 +23,21 @@ class DiceRoller : AppCompatActivity() {
 
     private fun rollDice()
     {
-        val resultText: TextView = findViewById(R.id.roll_result)
-
         val randomInt = Random().nextInt(6)+1
-        resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> {
+                R.drawable.dice_6
+            }
+        }
+        val diceImage: ImageView = findViewById(R.id.roll_result)
+
+        diceImage.setImageResource(drawableResource)
     }
 }
