8f12a57fa58c96b6a257d19183e7150915660230
asterixorobelix <nathanstasin@gmail.com>
2018-11-25 20:20:32 +0200

    on click listener for rollbutton

--- diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
+++ diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
@@ -2,11 +2,21 @@ package com.example.dell.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class DiceRoller : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dice_roller)
+
+        val rollButton: Button = this.findViewById(R.id.roll_button)
+        rollButton.text="Lets roll!"
+        rollButton.setOnClickListener()
+        {
+            Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
