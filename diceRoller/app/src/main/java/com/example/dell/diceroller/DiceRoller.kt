fdfbb5b45dee96b193ded5127e1c92a48d45d400
asterixorobelix <nathanstasin@gmail.com>
2018-11-25 18:18:10 +0200

    dice roller initial commit

---
+++ diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
@@ -0,0 +1,12 @@
+package com.example.dell.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DiceRoller : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dice_roller)
+    }
+}
