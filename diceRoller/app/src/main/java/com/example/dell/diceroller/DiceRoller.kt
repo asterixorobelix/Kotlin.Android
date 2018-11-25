3b7a218b1a31b4c5ff1a971e6cd39c25e5491c50
asterixorobelix <nathanstasin@gmail.com>
2018-11-25 20:25:46 +0200

    random dice roll results displayed

--- diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
+++ diceRoller/app/src/main/java/com/example/dell/diceroller/DiceRoller.kt
@@ -3,7 +3,9 @@ package com.example.dell.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class DiceRoller : AppCompatActivity() {
 
@@ -15,8 +17,15 @@ class DiceRoller : AppCompatActivity() {
         rollButton.text="Lets roll!"
         rollButton.setOnClickListener()
         {
-            Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT)
-                .show()
+            rollDice()
         }
+    }
+
+    private fun rollDice()
+    {
+        val resultText: TextView = findViewById(R.id.roll_result)
+
+        val randomInt = Random().nextInt(6)+1
+        resultText.text = randomInt.toString()
     }
 }
