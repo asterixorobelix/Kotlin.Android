afd8be43026b9fff28ad4e986c4ba0c1b91eb69f
Unknown <nathanstasin@gmail.com>
2019-01-03 12:12:22 +0200

    respond to button click and build intent

--- developer.android.google/basics/firstapp/app/src/main/java/asterixorobelix/firstapp/MainActivity.kt
+++ developer.android.google/basics/firstapp/app/src/main/java/asterixorobelix/firstapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package asterixorobelix.firstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "asterixorobelix.firstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View){
+        val editText= findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this,DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE,message) }
     }
 }
