a023b38003d0168b9e45fff1a2eae1f99208e351
Unknown <nathanstasin@gmail.com>
2019-01-03 14:04:59 +0200

    initial commit

---
+++ developer.android.google/basics/HelloJetpack/app/src/main/java/asterixorobelix/hellojetpack/StartActivity.kt
@@ -0,0 +1,19 @@
+package asterixorobelix.hellojetpack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import asterixorobelix.hellojetpack.ui.start.StartFragment
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, StartFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ developer.android.google/basics/HelloJetpack/app/src/main/java/asterixorobelix/hellojetpack/ui/start/StartViewModel.kt
@@ -0,0 +1,7 @@
+package asterixorobelix.hellojetpack.ui.start
+
+import android.arch.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
