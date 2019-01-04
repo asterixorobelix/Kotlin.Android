78dafacd4fc8645baca87822b62bac5704dee9bf
Unknown <nathanstasin@gmail.com>
2019-01-04 13:39:12 +0200

    implement databinding in mainActivity

--- udacity/DevelopingAndroidAppswithKotlin/AboutMe/app/src/main/java/asterixorobelix/aboutme/MainActivity.kt
+++ udacity/DevelopingAndroidAppswithKotlin/AboutMe/app/src/main/java/asterixorobelix/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package asterixorobelix.aboutme
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,21 +9,24 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import asterixorobelix.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.nickname_button).setOnClickListener{
+        binding.nicknameButton.setOnClickListener{
             addNickname(it)
         }
     }
 
     private fun addNickname(it: View) {
-        val textEntry = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_display)
+        val textEntry = binding.nicknameEdit
+        val nickNameTextView = binding.nicknameDisplay
 
         nickNameTextView.text = textEntry.text
 
